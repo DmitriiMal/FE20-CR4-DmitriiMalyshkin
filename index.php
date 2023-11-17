35908d0691044a85e77c4dd74897d3a42b8d82bd
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-17 12:04:40 +0100

    Displayed  List of Media

--- index.php
+++ index.php
@@ -7,12 +7,12 @@ $cards = "";
 if (mysqli_num_rows($result) > 0) {
   while ($row = mysqli_fetch_assoc($result)) {
     $cards .= "
-    <div>
-        <div class='card' style='width: 18rem;'>
-          <img src='...' class='card-img-top' alt='...'>
+    <div class='my-2'>
+        <div class='card'>
+          <img src='images/$row[image]' class='card-img-top' alt='...'>
           <div class='card-body'>
-            <h5 class='card-title'>Card title</h5>
-            <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class='card-title'>$row[title]</h5>
+            <p class='card-text'>$row[author_first_name] $row[author_first_name]</p>
             <a href='#' class='btn btn-primary'>Go somewhere</a>
           </div>
         </div>
@@ -36,7 +36,11 @@ if (mysqli_num_rows($result) > 0) {
 
 <body>
   <?php require_once "components/navbar.php" ?>
-
+  <div class="container">
+    <div class="row row-cols-xs-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3">
+      <?= $cards ?>
+    </div>
+  </div>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
