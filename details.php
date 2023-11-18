7fcba1c33f3334f51153669c7cfa3e9dd560d47b
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-18 15:54:50 +0100

    add links for styling

--- details.php
+++ details.php
@@ -9,34 +9,9 @@ if (isset($_GET['ISBN']) && !empty($_GET['ISBN'])) {
 
   $result = mysqli_query($connect, $sql);
 
-  $cards = "";
 
   if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-      $cards .= "
-    <div class='my-2'>
-        <div class='card'>
-          <img src='images/$row[image]' class='card-img-top' alt='...'>
-          <div class='card-body'>
-            <h5 class='card-title'>$row[title]</h5>
-            <p class='card-text'>$row[ISBN]</p>
-            <p class='card-text'>$row[author_first_name] $row[author_first_name]</p>
-            <p class='card-text'>$row[short_description]</p>
-            <p class='card-text'>$row[type]</p>
-            <a href='publisher.php?publisher_name=" . urlencode($row['publisher_name']) . "'>
-
-              <p class='card-text fs-6'>$row[publisher_name]</p>
-            </a>
-            <p class='card-text'>$row[publisher_address]</p>
-            <p class='card-text'>$row[publish_date]</p>
-            <a href='index.php' class='btn btn-dark'>Home</a>
-            <a href='update.php?ISBN=$row[ISBN]' class='btn btn-warning'>Edit</a>
-            <a href='delete.php?ISBN=$row[ISBN]' class='btn btn-danger'>Delete</a>
-          </div>
-        </div>
-    </div>
-    ";
-    }
+    $row = mysqli_fetch_assoc($result);
   }
 }
 
@@ -50,17 +25,41 @@ mysqli_close($connect);
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <title>Library</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="navbar.css">
+  <script src="https://kit.fontawesome.com/553d5d3b41.js" crossorigin="anonymous"></script>
+  <link rel="icon" type="image/x-icon" href="favicon.ico" />
+  <title>Details | LibraLink</title>
 </head>
 
 <body>
   <?= $navbar ?>
   <div class="container">
 
-    <?= $cards ?>
+    <div class='my-2'>
+      <div class='card'>
+        <img src='images/<?= $row['image'] ?>' class='card-img-top' alt='...'>
+        <div class='card-body'>
+          <h5 class='card-title'> <?= $row['title'] ?></h5>
+          <p class='card-text'><?= $row['ISBN'] ?></p>
+          <p class='card-text'><?= $row['author_first_name'] ?> <?= $row['author_first_name'] ?></p>
+          <p class='card-text'><?= $row['short_description'] ?></p>
+          <p class='card-text'><?= $row['type'] ?></p>
+          <a href='publisher.php?publisher_name=<?= urlencode($row['publisher_name']) ?>'>
+
+            <p class='card-text fs-6'><?= $row['publisher_name'] ?></p>
+          </a>
+          <p class='card-text'><?= $row['publisher_address'] ?></p>
+          <p class='card-text'><?= $row['publish_date'] ?></p>
+          <a href='index.php' class='btn btn-dark'>Home</a>
+          <a href='update.php?ISBN=$row[ISBN]' class='btn btn-warning'>Edit</a>
+          <a href='delete.php?ISBN=$row[ISBN]' class='btn btn-danger'>Delete</a>
+        </div>
+      </div>
+    </div>
 
   </div>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
 </html>
