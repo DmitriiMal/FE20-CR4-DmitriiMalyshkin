c03cc668febe9b85df49a8525c5cbef62c11d8c3
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2023-11-18 16:56:09 +0100

    change placeholder

--- index.php
+++ index.php
@@ -8,23 +8,29 @@ $cards = "";
 if (mysqli_num_rows($result) > 0) {
   while ($row = mysqli_fetch_assoc($result)) {
     $cards .= "
-    <div class='my-2'>
-        <div class='card'>
-          <img src='images/$row[image]' class='card-img-top' alt='...'>
-          <div class='card-body'>
-            <h5 class='card-title'>$row[title]</h5>
-            <p class='card-text'>$row[author_first_name] $row[author_first_name]</p>
-            <a href='details.php?ISBN=$row[ISBN]' class='btn btn-primary'>Details</a>
-            <a href='update.php?ISBN=$row[ISBN]' class='btn btn-warning'>Edit</a>
-            <a href='delete.php?ISBN=$row[ISBN]' class='btn btn-danger'>Delete</a>
-            
-            <!--  <a href='publisher.php?publisher_name=$row[publisher_name]'> -->
-            <a href='publisher.php?publisher_name=" . urlencode($row['publisher_name']) . "'>
+
+
+
+    <div class='m-03'>
+      <div class='card'>
+          
+          <img src='images/$row[image]' alt=''>
+          <div class='details'>
+              <label>$row[title]</label>
+              <p>$row[author_first_name] $row[author_last_name]</p>
+              <!--  <a href='publisher.php?publisher_name=$row[publisher_name]'> -->
+            <a class='pub-link' href='publisher.php?publisher_name=" . urlencode($row['publisher_name']) . "'>
 
               <p class='card-text fs-6'>$row[publisher_name]</p>
             </a>
+
+              <a href='details.php?ISBN=$row[ISBN]' class='btn btn-primary'>Details</a>
+              <a href='update.php?ISBN=$row[ISBN]' class='btn btn-secondary'>Edit</a>
+              <a href='delete.php?ISBN=$row[ISBN]' class='btn btn-tertiary'>Delete</a>
+
+
           </div>
-        </div>
+      </div>
     </div>
     ";
   }
@@ -50,8 +56,8 @@ mysqli_close($connect);
 
 <body>
   <?= $navbar ?>
-  <div class="container">
-    <div class="row row-cols-xs-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3">
+  <div class="container pt-120">
+    <div class="grid-container">
       <?= $cards ?>
     </div>
   </div>
